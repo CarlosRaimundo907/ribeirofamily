43934103994c23d6c4d59bfcdcc92687e9111481
CarlosRaimundo907 <ribeiroraimundo63@gmail.com>
2025-10-03 19:37:53 -0300

    commit de 3Out 1930hs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { Contacts } from './contacts/contacts';
+import { About } from './about/about';
+
+// Nome do site para compor o `title`
+const siteName = "Ribeiro Family";
+
+export const routes: Routes = [
+        // Rotas vazias redirecionam para a `/home` ← Sempre a primeira
+        { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+        // Páginas { rota, componente, título }
+        { path: 'home', component: Home, title: siteName },
+        { path: 'contacts', component: Contacts, title: `${siteName} - Faça Contato` },
+        { path: 'about', component: About, title: `${siteName} - Sobre` },
+    
+        // Rota coringa para redirecionar caminhos inválidos ← Sempre a última
+        { path: '**', redirectTo: '/home' }
+
+];
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
+// src/app/app.ts
+
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ribeiroFamily');
+  protected readonly title = signal('ngcrud');
 }
