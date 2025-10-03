03b8de59282343714ae4d16f44093aa9b79b2f63
CarlosRaimundo907 <ribeiroraimundo63@gmail.com>
2025-10-03 19:55:48 -0300

    Autenticação OK Firebase

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,26 @@
 // src/app/app.ts
 
-import { Component, signal } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { AuthService } from './services/auth-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('ngcrud');
+  title = 'ngcrud';
+
+  constructor(public authService: AuthService) { }
+
+  // Método para confirmar e fazer logout
+  confirmLogout() {
+    if (confirm('Tem certeza que deseja sair?')) {
+      this.authService.logout();
+    }
+  }
 }
