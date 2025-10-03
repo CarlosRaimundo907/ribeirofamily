2cc9a395d53fa35c307fe8113f2466febc48c2dc
Carlos Raimundo <carlosraimundo@MacBook-Air-de-Carlos.local>
2025-10-02 23:49:04 -0300

    primeiro commit do projeto ribeiroFamily

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,23 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({ 
+        projectId: "ribeirofamily-ac13e", 
+        appId: "1:529531568319:web:3ebcc1961f8ec1f15f860a", 
+        storageBucket: "ribeirofamily-ac13e.firebasestorage.app", 
+        apiKey: "AIzaSyB-67wYkv2JRM8Q6YeBu9Cfg-RgJk8kxUU", 
+        authDomain: "ribeirofamily-ac13e.firebaseapp.com" 
+    })), 
+    provideAuth(() => getAuth()), 
+    provideFirestore(() => getFirestore())
   ]
 };
